d017075a4734ad376bc97bc742d00af69e2a042a
StarGale <zhenchix@hotmail.com>
2018-05-04 14:47:17 -0700

    update

--- app.js
+++ app.js
@@ -12,14 +12,17 @@ Test this function by hand in the console to get it working, and when you think 
 function sum(a, b) { //eslint-disable-line
 
 
-var result = a + b;
-var message = 'The sum of ' + a + ' and ' + b + ' is ' + result + '.';
+    var result = a + b;
+    var message = 'The sum of ' + a + ' and ' + b + ' is ' + result + '.';
+    // document.getElementById('sum').innerHTML = result;
 
 }
 return [result, message];
 
+
+
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -33,11 +36,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
+var result = a * b;
+var message ='The product of'+ a +'and' + b +' is' + result + '.';
 
 }
+return [result,message];
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -54,11 +60,16 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+var sumResult = a + b + c;
+var multiplyResult = a * b * c;
+var message = a +' and'+ b +' and'+ c + 'sum to' + sumResult +' .';
+var message ='The product of' + a + 'and' + b + 'and' + c + 'is' + multiplyResult + '.';
 
 }
 
+return[result,message];
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -132,3 +143,4 @@ function multiplyAnyArray(dynamicArray) { //eslint-disable-line
 // testMultiplyAnyArray(testDynamicArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
+
